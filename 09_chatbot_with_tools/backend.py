52ff36d9bd836b2b65890389a4afb6bb3e7d0690
Ijaz Ullah Khan <ijazullahkhan09@gmail.com>
2026-02-09 10:09:34 +0500

    make changes for deployment

--- 09_chatbot_with_tools/backend.py
+++ 09_chatbot_with_tools/backend.py
@@ -2,7 +2,7 @@ from langgraph.graph import StateGraph, START
 from typing import TypedDict, Annotated
 from langchain_core.messages import BaseMessage
 from langchain_groq import ChatGroq
-from langgraph_checkpoint.sqlite import SqliteSaver
+from langgraph.checkpoint.sqlite import SqliteSaver
 from langgraph.graph.message import add_messages
 from langgraph.prebuilt import ToolNode, tools_condition
 from langchain_community.tools import DuckDuckGoSearchRun
